f47f4ff72c499b0c3dde52ee30eacf5408fb3b38
Henrik <henrik_mathiesen@hotmail.com>
2015-08-23 15:11:45 +0200

    context-type should be content-type, also trying json response

--- server.js
+++ server.js
@@ -1,28 +1,29 @@
 var http = require('http');
+var fs = require('fs');
 
+var events = require('./data/event/1.json');
 
 var routes = {
 	getEvents: "/getevents"
 };
 
 var send404Response = function (res) {
-	res.writeHead(404, { 'Context-Type': 'text/plain' });
+	res.writeHead(404, { 'Content-Type': 'text/plain' });
 	res.write('404: Not found');
 	res.end();
 };
 
 var sendEvents = function (res) {
-	
 	res.setHeader("Access-Control-Allow-Origin", "http://localhost:1337");
 	res.setHeader("Access-Control-Allow-Headers", "Content-Type");
 	
-	res.writeHead(200, { 'Context-Type': 'text/plain' });
-	res.write('Hi there!');
+	res.writeHead(200, { 'Content-Type': 'application/json' });
+	res.write(JSON.stringify(events));
 	res.end();
 };
 
 http.createServer(function (req, res) {
-
+	
 	if((req.method == 'GET') && (req.url == routes.getEvents)) {
 		sendEvents(res);
 	}
