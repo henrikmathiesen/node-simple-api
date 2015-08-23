defbac3095c2cfca2418316d69bd52627bb29c6b
Henrik <henrik_mathiesen@hotmail.com>
2015-08-23 14:21:39 +0200

    reworked simple routing

--- server.js
+++ server.js
@@ -1,9 +1,30 @@
 var http = require('http');
 
-http.createServer(function(req, res){
-	
-	res.writeHead(200, {'Context-Type': 'text/plain'});
+
+var routes = {
+	getEvents: "/getevents"
+};
+
+var send404Response = function (res) {
+	res.writeHead(200, { 'Context-Type': 'text/plain' });
+	res.write('404: Not found');
+	res.end();
+};
+
+var sendEvents = function (res) {
+	res.writeHead(200, { 'Context-Type': 'text/plain' });
 	res.write('Hi there!');
 	res.end();
-	
+};
+
+http.createServer(function (req, res) {
+
+	if((req.method == 'GET') && (req.url == routes.getEvents)) {
+		sendEvents(res);
+	}
+	else {
+		send404Response(res);
+	}
+
+
 }).listen(1338);
