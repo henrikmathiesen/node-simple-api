3fbffb172b8482f0551d113556667a7a38d7f3e4
Henrik <henrik_mathiesen@hotmail.com>
2015-08-23 13:52:51 +0200

    Simple request and response

--- server.js
+++ server.js
@@ -1,1 +1,9 @@
 var http = require('http');
+
+http.createServer(function(req, res){
+	
+	res.writeHead(200, {'Context-Type': 'text/plain'});
+	res.write('Hi there!');
+	res.end();
+	
+}).listen(1338);
