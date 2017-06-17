4cd5919be52669d6fea9dab1f29251db3b5206f9
henrikmathiesen <henrik_mathiesen@hotmail.com>
2017-06-17 09:33:16 +0200

    -removed iife, not needed in a module

--- events.js
+++ events.js
@@ -1,57 +1,53 @@
-(function () {
+var events = [
 
-        var events = [
-
-                {
-                        "name": "Angular Boot Camp",
-                        "id": 0,
-                        "date": "1/1/2014",
-                        "time": "10:30 am",
-                        "location": {
-                                "address": "Google Headquarters",
-                                "city": "Mountain View",
-                                "province": "CA"
-                        },
-                        "imageUrl": "http://pascalprecht.github.com/slides/angularjs-insights/img/angularjs-logo.png",
-                        "sessions": [
-                                {
-                                        "id": 1,
-                                        "name": "Directives Masterclass",
-                                        "creatorName": "Bob Smith",
-                                        "duration": 1,
-                                        "level": "Advanced",
-                                        "abstract": "In this session you will learn the ins and outs of directives!",
-                                        "upVoteCount": 0
-                                },
-                                {
-                                        "id": 2,
-                                        "name": "Scopes for fun and profit",
-                                        "creatorName": "John Doe",
-                                        "duration": 3,
-                                        "level": "Introductory",
-                                        "abstract": "This session will take a closer look at scopes.  Learn what they do, how they do it, and how to get them to do it for you.",
-                                        "upVoteCount": 0
-                                },
-                                {
-                                        "id": 3,
-                                        "name": "Well Behaved Controllers",
-                                        "creatorName": "Jane Doe",
-                                        "duration": 2,
-                                        "level": "Intermediate",
-                                        "abstract": "Controllers are the beginning of everything Angular does.  Learn how to craft controllers that will win the respect of your friends and neighbors.",
-                                        "upVoteCount": 0
-                                }
-                        ]
+        {
+                "name": "Angular Boot Camp",
+                "id": 0,
+                "date": "1/1/2014",
+                "time": "10:30 am",
+                "location": {
+                        "address": "Google Headquarters",
+                        "city": "Mountain View",
+                        "province": "CA"
                 },
+                "imageUrl": "http://pascalprecht.github.com/slides/angularjs-insights/img/angularjs-logo.png",
+                "sessions": [
+                        {
+                                "id": 1,
+                                "name": "Directives Masterclass",
+                                "creatorName": "Bob Smith",
+                                "duration": 1,
+                                "level": "Advanced",
+                                "abstract": "In this session you will learn the ins and outs of directives!",
+                                "upVoteCount": 0
+                        },
+                        {
+                                "id": 2,
+                                "name": "Scopes for fun and profit",
+                                "creatorName": "John Doe",
+                                "duration": 3,
+                                "level": "Introductory",
+                                "abstract": "This session will take a closer look at scopes.  Learn what they do, how they do it, and how to get them to do it for you.",
+                                "upVoteCount": 0
+                        },
+                        {
+                                "id": 3,
+                                "name": "Well Behaved Controllers",
+                                "creatorName": "Jane Doe",
+                                "duration": 2,
+                                "level": "Intermediate",
+                                "abstract": "Controllers are the beginning of everything Angular does.  Learn how to craft controllers that will win the respect of your friends and neighbors.",
+                                "upVoteCount": 0
+                        }
+                ]
+        },
 
-                { "id": 1, "creator": "jmcooper", "name": "Code Camp", "date": "03/15/2013", "time": "9:00am - 5:00pm", "location": { "address": "123 Wall St", "city": "New York", "province": "NY" }, "imageUrl": "http://blog.laptopmag.com/wpress/wp-content/uploads/2012/08/Code-Camp_sf.jpg", "sessions": [{ "id": 1, "upVoteCount": 0, "creator": "jmcooper", "name": "How To Program", "track": "DevOps", "duration": 1, "abstract": "this session will teach you to program", "creatorName": "Jim Cooper", "level": "advanced" }, { "id": 2, "upVoteCount": 0, "creator": "jmcooper", "name": "How To Dance", "track": "Web", "duration": 4, "abstract": "this session will teach you to dance", "creatorName": "Jim Cooper", "level": "introductory" }, { "id": 3, "upVoteCount": 0, "creator": "bob", "name": "How To Sing", "track": "DevOps", "duration": 1, "abstract": "this session will teach you to sing  Lorum Ipsum FooBarsum Lorum Ipsum FooBarsum Lorum Ipsum FooBarsum Lorum Ipsum FooBarsum Lorum Ipsum FooBarsum Lorum Ipsum FooBarsum Lorum Ipsum FooBarsum Lorum Ipsum FooBarsum Lorum Ipsum FooBarsum  Lorum Ipsum FooBarsum Lorum Ipsum FooBarsum Lorum Ipsum FooBarsum Lorum Ipsum FooBarsum Lorum Ipsum FooBarsum Lorum Ipsum FooBarsum Lorum Ipsum FooBarsum Lorum Ipsum FooBarsum Lorum Ipsum FooBarsum  Lorum Ipsum FooBarsum Lorum Ipsum FooBarsum Lorum Ipsum FooBarsum Lorum Ipsum FooBarsum Lorum Ipsum FooBarsum Lorum Ipsum FooBarsum Lorum Ipsum FooBarsum Lorum Ipsum FooBarsum Lorum Ipsum FooBarsum ", "level": "advanced", "creatorName": "Bob" }, { "duration": 2, "name": "How To Be Awesome", "abstract": "this course will make me rich", "creator": "bob", "creatorName": "Bob", "id": 4, "upVoteCount": 0, "track": "Web", "level": "introductory" }, { "duration": 2, "track": "Web", "name": "How to build Angular Applications", "abstract": "This session is all about angular applications", "creator": "bob", "creatorName": "Bob", "upVoteCount": 0, "id": 5, "level": "intermediate" }] },
-                
-                {"id":2,"creator":"joeeames","name":"Code Retreat","date":"03/16/2013","time":"8:00am - 4:30pm","location":{"address":"42 Wallaby Way","city":"Sydney","province":"AU"},"imageUrl":"http://api.ning.com/files/2vzV35vB8k5SPm92v9bGA0ng9DP2h-ONIcgPoDUThiIk1roHMF4eVjzlN2o3z9S5aU*wKgn9jZpdxEekNQCYvnOumr-beV44/cnlogo.png"},
-                
-                {"name":"fall code camp","date":"1/1/2010","time":"10am","location":{"address":"123 fake st","city":"riverton","province":"ut"},"imageUrl":"http://s3.favim.com/orig/39/funny-llama-lol-nonsense-pink-Favim.com-325386.jpg","creator":"jmcooper","id":3}
-
-        ];
+        { "id": 1, "creator": "jmcooper", "name": "Code Camp", "date": "03/15/2013", "time": "9:00am - 5:00pm", "location": { "address": "123 Wall St", "city": "New York", "province": "NY" }, "imageUrl": "http://blog.laptopmag.com/wpress/wp-content/uploads/2012/08/Code-Camp_sf.jpg", "sessions": [{ "id": 1, "upVoteCount": 0, "creator": "jmcooper", "name": "How To Program", "track": "DevOps", "duration": 1, "abstract": "this session will teach you to program", "creatorName": "Jim Cooper", "level": "advanced" }, { "id": 2, "upVoteCount": 0, "creator": "jmcooper", "name": "How To Dance", "track": "Web", "duration": 4, "abstract": "this session will teach you to dance", "creatorName": "Jim Cooper", "level": "introductory" }, { "id": 3, "upVoteCount": 0, "creator": "bob", "name": "How To Sing", "track": "DevOps", "duration": 1, "abstract": "this session will teach you to sing  Lorum Ipsum FooBarsum Lorum Ipsum FooBarsum Lorum Ipsum FooBarsum Lorum Ipsum FooBarsum Lorum Ipsum FooBarsum Lorum Ipsum FooBarsum Lorum Ipsum FooBarsum Lorum Ipsum FooBarsum Lorum Ipsum FooBarsum  Lorum Ipsum FooBarsum Lorum Ipsum FooBarsum Lorum Ipsum FooBarsum Lorum Ipsum FooBarsum Lorum Ipsum FooBarsum Lorum Ipsum FooBarsum Lorum Ipsum FooBarsum Lorum Ipsum FooBarsum Lorum Ipsum FooBarsum  Lorum Ipsum FooBarsum Lorum Ipsum FooBarsum Lorum Ipsum FooBarsum Lorum Ipsum FooBarsum Lorum Ipsum FooBarsum Lorum Ipsum FooBarsum Lorum Ipsum FooBarsum Lorum Ipsum FooBarsum Lorum Ipsum FooBarsum ", "level": "advanced", "creatorName": "Bob" }, { "duration": 2, "name": "How To Be Awesome", "abstract": "this course will make me rich", "creator": "bob", "creatorName": "Bob", "id": 4, "upVoteCount": 0, "track": "Web", "level": "introductory" }, { "duration": 2, "track": "Web", "name": "How to build Angular Applications", "abstract": "This session is all about angular applications", "creator": "bob", "creatorName": "Bob", "upVoteCount": 0, "id": 5, "level": "intermediate" }] },
         
-        module.exports = events;
+        {"id":2,"creator":"joeeames","name":"Code Retreat","date":"03/16/2013","time":"8:00am - 4:30pm","location":{"address":"42 Wallaby Way","city":"Sydney","province":"AU"},"imageUrl":"http://api.ning.com/files/2vzV35vB8k5SPm92v9bGA0ng9DP2h-ONIcgPoDUThiIk1roHMF4eVjzlN2o3z9S5aU*wKgn9jZpdxEekNQCYvnOumr-beV44/cnlogo.png"},
+        
+        {"name":"fall code camp","date":"1/1/2010","time":"10am","location":{"address":"123 fake st","city":"riverton","province":"ut"},"imageUrl":"http://s3.favim.com/orig/39/funny-llama-lol-nonsense-pink-Favim.com-325386.jpg","creator":"jmcooper","id":3}
 
-})();
+];
+
+module.exports = events;
